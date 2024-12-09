a3fc678d429fc7b8f8af090a56f05e7d9fbb1751
Ryan Huellen <me@ryanhuellen.com>
2024-12-08 19:58:19 -0600

    fix: exporting `isRelationshipObject`

--- src/serializers/utils/index.ts
+++ src/serializers/utils/index.ts
@@ -3,5 +3,6 @@ export * from './getMetadataBySymbol';
 export * from './collect';
 export * from './clean';
 export * from './isObject';
+export * from './isRelationshipObject';
 export * from './isResourceIdentifierObject';
 export * from './isResourceLinkage';
